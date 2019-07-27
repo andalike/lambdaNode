a5710dc3a4755e93467009faaa727345b0e9e342
andalike <43137695+andalike@users.noreply.github.com>
2019-07-27 22:04:38 +0530

    Code looks better

--- startStopInstance.js
+++ startStopInstance.js
@@ -3,65 +3,40 @@ AWS.config.update({region: 'us-east-1'});
 var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
 function instanceOperations () {
-var instanceID='i-07bcd8d2b5e08c6e4';
+var instanceID='i-0e0148ef75183e4c1';
 var status='START';
 
 var params = {
-  InstanceIds: [instanceID],
-  DryRun: true
+  InstanceIds: [instanceID]
 };
 
 if (status.toUpperCase() === "START") {
-  ec2.stopInstances(params, function(err, data) {
-    if (err && err.code === 'DryRunOperation') {
-      params.DryRun = false;
       ec2.startInstances(params, function(err, data) {
           if (err) {
-            console.log("Error in Stopping Instance->" + err);
+            console.log("Error in Starting Instance->" + err);
           } else if (data) {
             console.log("Success in Starting Instane->", data.StartingInstances);
           }
       });
-    } else {
-      console.log("You don't have permission to start instances.");
-    }
-  });
 } else if (status.toUpperCase() === "STOP") {
-  ec2.stopInstances(params, function(err, data) {
-    if (err && err.code === 'DryRunOperation') {
-      params.DryRun = false;
       ec2.stopInstances(params, function(err, data) {
           if (err) {
             console.log("Error in Stopping Instance->" + err);
           } else if (data) {
-            console.log("Success in Starting Instane->", data.StoppingInstances);
+            console.log("Success in Stopping Instane->", data.StoppingInstances);
           }
       });
-    }
-
-    else {
-      console.log("You don't have permission to stop instances");
-    }
-  });
 }
 else if (status.toUpperCase() === "REBOOT") {
-    ec2.stopInstances(params, function(err, data) {
-      if (err && err.code === 'DryRunOperation') {
-        params.DryRun = false;
+
         ec2.stopInstances(params, function(err, data) {
             if (err) {
-              console.log("Error in Stopping Instance->" + err);
+              console.log("Error in Rebooting Instance->" + err);
             } else if (data) {
-              console.log("Success in Starting Instane->", data.StoppingInstances);
+              console.log("Success in Rebooting Instane->", data.StoppingInstances);
             }
         });
       }
-
-      else {
-        console.log("You don't have permission to stop instances");
-      }
-    });
-  }
 }
 
 module.exports.handler = instanceOperations;
